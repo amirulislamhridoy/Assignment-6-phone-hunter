ea9be1efbd73a17406d095166d884365a4b7c10c
amirulislamhridoy <hossainhridoy30687@gmail.com>
2022-03-01 01:32:33 +0600

    second

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('ah')
+const load = async () => {
+    const inputField = document.getElementById('input-field')
+    const inputText = inputField.value 
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`
+    const res = await fetch(url)
+    const data = await res.json()
+    display(data.data)
+}
+
+const display = (phones) => {
+    // console.log(phones)
+    document.getElementById('row').textContent = ''
+    if(phones.length > 5){
+        for(let i=0; i<20; i++){
+            const phone = phones[i]
+            createElement(phone)
+        }
+    }
+    else{
+        for(let phone of phones){
+            // console.log(phone)
+            createElement()
+        }
+    }
+}
+
+const createElement = (phone) => {
+    console.log(phone)
+    const row = document.getElementById('row')
+    const div = document.createElement('div')
+    div.classList.add('col')
+            div.innerHTML = `
+                <div onclick="" class="card rounded-3">
+                  <img src="${phone.image}" class="card-img-top p-5" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand}</p>
+                    <button class="btn btn-primary">Details</button>
+                  </div>
+                </div>
+            `
+            row.appendChild(div)
+}
